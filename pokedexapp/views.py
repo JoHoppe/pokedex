1f34b3e4347dea2f18953ae02e38dc404ed79558
JHoppe <sgs3.iloveit@gmail.com>
2023-08-03 10:14:13 +0200

    made the single view look nicer

--- pokedexapp/views.py
+++ pokedexapp/views.py
@@ -35,33 +35,27 @@ def show_one_pokemon(request, id):
 
     # Get the total number of Pokémon in the database
     total_pokemons = Pokemon.objects.count()
-    if id > total_pokemons:
+    if id > total_pokemons or id <= 0:
         return render(request, "pokedexapp/404.html")
 
+    # Get the current and adjacent Pokemon
     pokemon = get_object_or_404(Pokemon, id=id)
-
-    # Encode the image data of the sprite to base64 if available
-    if pokemon.sprite and pokemon.sprite.image:
-        image_data_base64 = base64.b64encode(pokemon.sprite.image).decode('utf-8')
-        pokemon.sprite.image_data_base64 = image_data_base64
-    else:
-        pokemon.sprite.image_data_base64 = None
-
     prev_pokemon = get_object_or_404(Pokemon, id=id - 1) if id > 1 else None
     next_pokemon = get_object_or_404(Pokemon, id=id + 1) if id < total_pokemons else None
 
-    if prev_pokemon and prev_pokemon.sprite and prev_pokemon.sprite.image:
-        prev_pokemon.sprite.image_data_base64 = base64.b64encode(prev_pokemon.sprite.image).decode('utf-8')
-    else:
-        prev_pokemon.sprite.image_data_base64 = None
+    # Encode the image data of the sprite to base64
+    image_data_base64 = base64.b64encode(pokemon.sprite.image).decode('utf-8')
+    pokemon.sprite.image_data_base64 = image_data_base64
 
-    if next_pokemon and next_pokemon.sprite and next_pokemon.sprite.image:
+    if prev_pokemon:
+        prev_pokemon.sprite.image_data_base64 = base64.b64encode(prev_pokemon.sprite.image).decode('utf-8')
+    if next_pokemon:
         next_pokemon.sprite.image_data_base64 = base64.b64encode(next_pokemon.sprite.image).decode('utf-8')
-    else:
-        next_pokemon.sprite.image_data_base64 = None
 
     pokemon.type_name = get_str_type(pokemon.get_type())
     pokemon.type_name = list(pokemon.type_name)
+
+
 
     context = {
         "pokemon": pokemon,
