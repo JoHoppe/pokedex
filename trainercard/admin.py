bb2a8d7411d11314a3d6eb210ee229bc33c68ea0
JHoppe <sgs3.iloveit@gmail.com>
2023-10-23 11:07:35 +0200

    Now models works properly and poketeams are added on account creation

--- trainercard/admin.py
+++ trainercard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import TrainerCard
+from .models import TrainerCard,PokeTeam
 
 admin.site.register(TrainerCard)
-
+admin.site.register(PokeTeam)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from trainercard.models import TrainerCard
+from trainercard.models import TrainerCard,PokeTeam
 
 
